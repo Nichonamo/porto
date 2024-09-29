4285617959842d4e509c74522b5944ccd122a7f8
Nicholas <161582389+Nichonamo@users.noreply.github.com>
2024-09-30 01:16:46 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// Change background dynamically when clicking a link
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+        // Get the id of the section
+        const sectionId = this.getAttribute("href").substring(1);
+
+        // Dynamically change the background image for each section
+        switch (sectionId) {
+            case 'about':
+                document.body.style.backgroundImage = 'url("images/about-bg.jpg")';
+                break;
+            case 'skills':
+                document.body.style.backgroundImage = 'url("images/skills-bg.jpg")';
+                break;
+            case 'projects':
+                document.body.style.backgroundImage = 'url("images/projects-bg.jpg")';
+                break;
+            case 'hobbies':
+                document.body.style.backgroundImage = 'url("images/hobbies-bg.jpg")';
+                break;
+            case 'contact':
+                document.body.style.backgroundImage = 'url("images/contact-bg.jpg")';
+                break;
+            default:
+                document.body.style.backgroundImage = 'none';
+        }
+    });
+});
