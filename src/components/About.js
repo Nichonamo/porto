8610214e74293dd05b6a193b141a7b0e0cd30b2f
Nicholas <nicholaskentx@gmail.com>
2024-10-15 11:55:30 +0700

    v2.1

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id="about">
       <h2>About Me</h2>
-      <p>Hello! I'm Nicholas Kent, a passionate software engineering student experienced in creating private servers, Software Engineering, and Web development.</p>
+      <p>My name is Nicholas Kent Unamo, a fifth-semester Software Engineering student at BINUS University Bandung. I am passionate about exploring and applying software development techniques in real-world contexts, and I enjoy gaining hands-on experience with various tools and technologies. Throughout my studies, I have developed practical skills in Software Engineering, Computer Networks, Algorithms & Programming, and Artificial Intelligence, which, alongside a strong theoretical foundation, enhance my growth in the technology sector. I aim to connect with others in the field and contribute to communities, especially those with limited access to technology, to foster growth and future advancements.</p>
     </section>
   );
 };
